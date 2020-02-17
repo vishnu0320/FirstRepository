5f4cbeab61ce8923c51f7886b7ed655d094e45a3
Vishnu <vishnu.gupta@daffodilsw.com>
2020-02-17 15:16:18 +0530

    auth project done

---
+++ auth/schema.js
@@ -0,0 +1,8 @@
+const mongoose=require('mongoose');
+const userSchema = new mongoose.Schema({
+    username: String,
+    googleID:String,
+  });
+  const userModel=mongoose.model('googleUser',userSchema);
+  
+  module.exports=userModel;
